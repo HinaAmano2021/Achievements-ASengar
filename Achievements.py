bedef6cb117b9bb996c2b4794a31772e3ef53e2f
HinaAmano2021 <89646207+HinaAmano2021@users.noreply.github.com>
2022-09-05 22:07:17 +0530

    Add files via upload

---
+++ Achievements.py
@@ -0,0 +1,33 @@
+class School_Responsibilities():
+    def year_2018(self):
+        print("\n","Events happened in 2018 in Barrackpore, West Bengal","\n")
+        print("I became the Jr. Head Girl in 2018 after many interviews, tests and hard work.")
+        
+    def year_2022(self):
+        print("\n","Got selected in NCC(National Cadet Cops) after agility, statstic, logical reasoning and decipline test.")
+        
+class Olympiad():
+    def year_2018(self):
+        print("\n","Events happened in medical 2018","\n")
+        print("Won gold medals in all subjects and one silver medal in logical reasoning.")
+        
+    def year_2022(self):
+        print("\n","Have participated in Olympiad in IMO, IGKO, ISSO & NSO and will work hard to achieve my goal.")
+        
+class Music():
+    def year_2018(self):
+        print("\n","Started learning Guitar and mastered some grammar.")
+    def year_2022(self):
+        print("\n","Have started learning BackPipe and have mastered 1st Scale, 2nd Scale, 3rd scale & Vande Mataram Intro.")
+        
+responsibilities = School_Responsibilities()
+responsibilities.year_2018()
+responsibilities.year_2022()
+
+olympiad = Olympiad()
+olympiad.year_2018()
+olympiad.year_2022()
+
+music = Music()
+music.year_2018()
+music.year_2022()
